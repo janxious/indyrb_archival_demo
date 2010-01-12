416499c8a2f8baefb2ae5a6c12347ee31fe8a204
Joel Meador <joel@expectedbehavior.com>
2010-01-12 13:54:31 -0500

    inconsistent interface tests

--- test/unit/foo_test.rb
+++ test/unit/foo_test.rb
@@ -1,8 +1,53 @@
 require 'test_helper'
 
 class FooTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup :set_us_up_some_bomb
+  
+  def set_us_up_some_bomb
+    Bar.delete_all!
+    Foo.delete_all!
+    @f  = Foo.create(:name => "foo0")
+    @b0 = @f.bars.create(:name => "bar0")
+    @b1 = @f.bars.create(:name => "bar1")
   end
+
+#   test "inconsistent interface, part 1" do
+#     @f.destroy
+#     assert @f.destroyed?, "I just destroyed this!"
+#     #assert @f.deleted?, "I just destroyed this!"
+#   end
+
+#   test "inconsistent interface, part 2" do
+#     @f.destroy
+#     assert Foo.find_with_destroyed(:all), "There should like... be at least one, maaaaan"
+#     #assert Foo.find_with_deleted(:all), "There should like... be at least one, maaaaan"
+#   end
+  
+#   test "inconsistent interface, part 3" do
+#     @f.delete
+#     #@f.delete!
+#     assert_equal 0, Foo.find_with_deleted(:all).size, "Let's view that thing I just deleted"
+#   end
+  
+#   test "inconsistent interface, part 4" do
+#     Foo.destroy_all
+#     assert_equal 0. Foo.find_with_deleted(:all).size, "Oh, I don't know what this shit is"
+#   end
+  
+#   test "inconsistent interface, part 5" do 
+#     Foo.delete_all
+#     #Foo.delete_all!
+#     assert_equal 0, Foo.find_with_deleted(:all).size, "Oh, I guess this actually calls destroy"
+#   end
+  
+#   test "inconsistent interface, part 6" do
+#     @f.destroy
+#     @f.undestroy
+# #     @f.undelete
+# #     @f.recover
+# #     @f.recover!
+#     assert_equal 1, Foo.all.size, "GIVE ME BACK MAH DATAZ"
+#   end
+  
+  
 end
